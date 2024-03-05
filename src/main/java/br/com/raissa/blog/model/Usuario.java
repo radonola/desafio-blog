60d037c007bcfb4eecea77d3f0a5849ca22337f1
Raissa Donola <radonola@gmail.com>
2024-03-05 10:28:37 -0300

    Implementação das classes Usuario e Role

---
+++ src/main/java/br/com/raissa/blog/model/Usuario.java
@@ -0,0 +1,56 @@
+package br.com.raissa.blog.model;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "usuario")
+public class Usuario {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(length = 100, nullable = false, unique = true)
+	private String email;
+	
+	@Column(length = 100, nullable = false)
+	private String nomeCompleto;
+	
+	@Column(length = 100, nullable = false)
+	private String senha;
+	
+	@Builder.Default
+	@ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	@JoinTable(name = "usuario_roles",
+		joinColumns = @JoinColumn(name = "usuario_id", referencedColumnName = "id"),
+		inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+	private List<Role> roles = new ArrayList<>();
+	
+	public void setRole(Role role) {
+
+		setRoles(Collections.singletonList(role));
+	}
+	
+}
